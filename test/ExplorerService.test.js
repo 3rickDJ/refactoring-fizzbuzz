e797a901ca0dfbabde57a473ab9587767243087d
3rick <erick.dejesus1@alumno.buap.mx>
2022-05-12 13:50:13 -0500

    test: ExplorerService.test

--- test/ExplorerService.test.js
+++ test/ExplorerService.test.js
@@ -3,7 +3,12 @@ const Reader = require('../lib/utils/Reader.js')
 describe('Unit tests for ExplorerService', () => {
   test('1) Static functions of class ExplorerService', () => {
     const explorers = Reader.readJsonFile('./explorers.json')
-    const explorersNode = ExplorerService.filterByMission(explorers, 'Node')
-    expect(explorersNode).toBeDefined()
+    const explorersInNode = ExplorerService.filterByMission(explorers, 'Node')
+    const explorersHowManyInNode = ExplorerService.getAmountOfExplorersByMission(explorers, 'Node')
+    const explorersInNodeUsernames = ExplorerService.getExplorersUsernamesByMission(explorers, 'node')
+    expect(explorersInNode[0].mission).toBe('node')
+    expect(explorersHowManyInNode).toBe(10)
+    expect(explorersInNodeUsernames[0].githubUsername).toBe('ajolonauta1')
+
   })
 })
